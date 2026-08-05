5b42adcf1be5e2c49d86623bb5b294c8d0cf2ce8
MatiGallegos <matiasgallegos2013@gmail.com>
2026-08-04 22:28:36 -0300

    feat: build note header dynamically

--- script.js
+++ script.js
@@ -1,19 +1,49 @@
 const newNoteButton = document.querySelector('.js-new-note-button');
 const notesList = document.querySelector('.js-notes-list');
 
+
+
 newNoteButton.addEventListener('click', createNote);
 
 function createNote() {
     const note = document.createElement('article');
+    const newHeader = document.createElement('header');
+    const titleInput = document.createElement('input');
+    const ChevronIcon = document.createElement('i');
+    
 
+    
     note.classList.add('note');
 
+    newHeader.classList.add('note-header');
+
+    ChevronIcon.classList.add('button-icon');
+    ChevronIcon.setAttribute("data-lucide", "chevron-down");
+
+    titleInput.classList.add('note-title');    
+    titleInput.setAttribute("type", "text");
+    titleInput.setAttribute("placeholder", "Título");
+    
+
+    newHeader.append(ChevronIcon);
+    newHeader.append(titleInput);
+    
+    note.append(newHeader);    
+
     notesList.append(note);
+
+    lucide.createIcons();
 
 } 
 
 
-/*
+
+
+
+/*Esta función crea una nueva carpeta en la lista de carpetas. 
+Se crea un elemento <li> y un <button> dentro de él, 
+y se agregan las clases correspondientes para el estilo. 
+Luego, se agrega la carpeta a la lista de carpetas en el DOM.
 
 const newFolderButton = document.querySelector('.js-new-folder-button');
 const folderList = document.querySelector('.js-folder-list');
@@ -29,7 +59,7 @@ function createFolder() {
 
     folder.append(folderButton);
     folderList.append(folder);
-    
+    folder.append(folderButton);
     
 }
 
